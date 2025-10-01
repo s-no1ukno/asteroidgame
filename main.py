1c1cb58a6e3d17ab517291bc35a3e2e222bf34fc
Jordan Snow <jordan@snowdev.co>
2025-09-30 21:56:53 -0600

    Constants and display All black display and game loop initialized

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import pygame
+from constants import *
+
+
 def main():
-    print("Hello from asteroidgame!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # initialization of game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
